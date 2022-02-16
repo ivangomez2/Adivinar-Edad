913001d40f8cbe60a712998dde3a1db10ebe022b
ivan <ivansangomez2@gmail.com>
2022-02-15 22:35:49 -0300

    agrego turnos

---
+++ indexDos.js
@@ -0,0 +1,8 @@
+let contador = 11;
+for (let i = 0; i <= 10; i++) {
+ let nombre = prompt("sistema de turnos - ingresa tu nombre")
+ contador--
+ alert("solo daremos" + " " + contador + " numeros" )
+ alert(nombre + " " + "tienes el turno numero"+ " " + i + " " + "Espera a ser atendido")
+
+}
